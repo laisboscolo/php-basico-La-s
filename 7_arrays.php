ef96d1ccdd07d622719ffa7f1bdff5c4306bc1fd
Laís Boscolo <costaboscololais2008@gmail.com>
2024-11-18 16:07:30 -0300

    atividade 7

--- 7_arrays.php
+++ 7_arrays.php
@@ -1,1 +1,26 @@
-<!-- digite o programa -->
+<!-- o programa exibe os dados recebidos em lista no formato tabela -->
+
+<?php
+//array associativo contendo informaçoes de produtos
+$produtos = [
+    ["nome"=>"Camiseta","preco"=>50.00, "quantidade" =>10],
+    ["nome"=>"Calça Jeans","preco"=>100.00,"quantidade"=>7],
+];
+
+//exibe os produtos em formato de tabela
+echo "<table border='1'>";
+
+//Titulo da tabela
+echo "<tr><th>Nome</th><th>Preço</th><th>Quantidade</th></tr>";
+
+// Para cada 'produto' do vetor 'produtos'
+foreach ($produtos as $produto) {
+    echo "<tr>";
+    echo "<td>" . $produto['nome'] . "</td>";
+    echo "<td>R$ " . number_format($produto['preco'], 2, ',', '.') . "</td>";
+    echo "<td>" . $produto['quantidade'] . "</td>";
+    echo "</tr>";
+}
+
+echo "</table>";
+?>
