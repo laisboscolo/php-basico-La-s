bae39a0fec7dadd8d2b4f830c0c6a7fd6666050e
Laís Boscolo <costaboscololais2008@gmail.com>
2024-11-25 08:37:10 -0300

    atividade 14

--- 14_upload.php
+++ 14_upload.php
@@ -20,10 +20,16 @@
 
     <?php
 
- 
-        // Digitar PHP (1º Aqui)
+                // Verifica se o formulário foi enviado 
+                if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-
+                    //Verifica se a pasta existe, caso não, cria pasta
+                    if (!is_dir($diretorio_destino)) {
+                        mkdir($diretorio_destino, 0777, true);
+                    } 
+                    $nome_arquivo = basename($_FILES['imagem']['name']);
+                    $caminho_completo = $diretorio_destino . $nome_arquivo;
+        
         // Move o arquivo enviado para o diretório de destino
         if (move_uploaded_file($_FILES['imagem']['tmp_name'], $caminho_completo)) {
             echo "<p>Upload realizado com sucesso!</p>";
@@ -31,7 +37,8 @@
         } else {
             echo "<p>Erro ao fazer upload do arquivo.</p>";
         }
-    
+    }
     ?>
+
 </body>
 </html>
