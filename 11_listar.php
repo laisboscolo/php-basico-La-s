255fd006eed0664c2e3c1512ec807aa9fc2bcad0
Laís Boscolo <costaboscololais2008@gmail.com>
2024-11-25 08:36:47 -0300

    atividade 11

--- 11_listar.php
+++ 11_listar.php
@@ -13,8 +13,35 @@ if ($conn->connect_error) {
 }
 
 
-// Digitar PHP + SQL (1º Aqui)
+// Consulta os dados da tabela clientes 
+$sql = "SELECT id, nome, email, telefone FROM clientes";
+$result = $conn->query($sql);
 
+// Verifique se existem registros e os exibe em formato de tabela
+if($result->num_rows > 0) {
+    echo "<h2>Lista de clientes cadastrados</h2>";
+
+    // define formato da tabela
+    echo "<table border='2'>";
+
+        // define cabeçalho da tabela 
+        echo "<tr><th>ID</th><th>Nomes</th><th>Email</th><th>Telefone</th></tr>";
+
+        // Enquanto tiver dados preenchidos no BD
+        // Listar e exibir em formato de tabela
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td>" . $row['id'] . "</td>";
+            echo "<td>" . $row['nome'] . "</td>";
+            echo "<td>" . $row['email'] . "</td>";
+            echo "<td>" . $row['telefone'] . "</td>";
+            echo "</tr>";
+        }
+    echo "</table>";
+
+} else {
+    echo "Nenhum cliente encontrado.";
+}
 
 // Fecha a conexão
 $conn->close();
