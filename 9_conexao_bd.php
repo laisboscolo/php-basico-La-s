474c7ef871eebe7d30871c3681879ab7f305f261
Laís Boscolo <costaboscololais2008@gmail.com>
2024-11-25 08:29:34 -0300

    atividade 9

--- 9_conexao_bd.php
+++ 9_conexao_bd.php
@@ -13,7 +13,31 @@
 
 <?php
 
-// Digitar PHP
+//ALTERAR de acordo com o número da porta
+// verificar conexão com o XAMPP
+
+use FFI\Exception;
+
+$servername = "localhost:3309";
+$username = "root";
+$password = "";
+$dbname = "exercicio";
+
+try {
+    // Tenta criar uma conexão com o banco de dados 
+    // Confere se as informações estão corretas
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Verifica se houve algum erro na conexão 
+    if($conn->connect_error) {
+        throw new Exception ("Falha na conexão: " . $conn->connect_error);
+    }
+    echo "Conexão realizado com sucesso!";
+} catch (Exception $e) {
+    // Exibe uma mensagem de erro amigável 
+    echo "Erro ao conectar ao banco de dados: " . $e->getMessage();
+}
+
 
 ?>
 
